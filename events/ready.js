be5d70f54a8a1e98e85cc59d422853ee1963ca70
Alexander <satan1c@ukr.net>
2023-02-22 00:14:48 +0200

    presence remove

--- events/ready.js
+++ events/ready.js
@@ -1,11 +1,8 @@
 import log from '../utils/log.js';
-import presenceController from '../utils/presence-controller.js';
 import registerCommands from '../utils/register-commands.js';
 
 export default async function (client) {
 	await registerCommands(client);
-	await presenceController(client)();
-	setInterval(presenceController(client), 30e3);
 
 	log(`{READY}`);
 }

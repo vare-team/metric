de1c1b7db15b42cfc0bb73022ad10b9cca0434aa
Alexander <satan1c@ukr.net>
2023-02-22 00:14:11 +0200

    presence update

--- utils/presence-controller.js
+++ utils/presence-controller.js
@@ -1,7 +1,5 @@
 import { ActivityType } from 'discord.js';
 
-let presence = 1;
-
 /**
  *
  * @param client {Client}
@@ -9,12 +7,7 @@ let presence = 1;
  */
 export default function (client) {
 	return async () => {
-		if (presence === 1) {
-			await client.guilds.fetch();
-			client.user.setActivity(`${client.guilds.cache.size} серверов | m.help`, { type: ActivityType.Watching });
-		} else if (presence === 2) {
-			client.user.setActivity(`m.help | m.stat`, { type: ActivityType.Listening });
-		}
-		presence++;
+		await client.guilds.fetch();
+		client.user.setActivity(`${client.guilds.cache.size} серверов`, { type: ActivityType.Watching });
 	};
 }

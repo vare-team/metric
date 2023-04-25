a90056d78330a22bbf7a572da41e11e841d36738
MegaVasiliy007 <megavasiliy007@vare.pw>
2023-04-25 20:27:23 +0300

    add activity

--- events/ready.js
+++ events/ready.js
@@ -1,8 +1,10 @@
 import log from '../utils/log.js';
 import registerCommands from '../utils/register-commands.js';
+import presenceController from '../utils/presence-controller.js';
 
 export default async function (client) {
 	await registerCommands(client);
+	await presenceController(client);
 
 	log(`{READY}`);
 }
--- utils/presence-controller.js
+++ utils/presence-controller.js
@@ -5,9 +5,6 @@ import { ActivityType } from 'discord.js';
  * @param client {Client}
  * @return {(function(): Promise<void>)|*}
  */
-export default function (client) {
-	return async () => {
-		await client.guilds.fetch();
-		client.user.setActivity(`${client.guilds.cache.size} серверов`, { type: ActivityType.Watching });
-	};
+export default async function (client) {
+	await client.user.setActivity(`на статистику`, { type: ActivityType.Watching });
 }

339679e8e7c6c806f9938a3818bb93a8cd490fa5
Alexander <satan1c@ukr.net>
2023-05-27 22:51:07 +0300

    replace db update methods

--- api/controllers/ups-update.js
+++ api/controllers/ups-update.js
@@ -1,7 +1,12 @@
 import SdcStat from '../models/sdc-stat.js';
-import { literal } from 'sequelize';
 
 export default async function (req, res) {
-	await SdcStat.upsert({ date: new Date(), ups: literal('COALESCE(ups, 0) + 1') });
+	const upTime = new Date();
+	const [stat, created] = await SdcStat.findOrCreate({
+		where: { date: upTime },
+		defaults: { ups: 1 },
+	});
+
+	if (!created) await stat.increment(['ups']);
 	res.end();
 }

2bee2fc7ade3837cdb24a467b99f5aa1603802aa
Alexander <satan1c@ukr.net>
2023-05-29 20:47:48 +0300

    add category validation

---
+++ api/configs/count-categories.js
@@ -0,0 +1,1 @@
+export default ['added', 'removed', 'guilds', 'ups'];
--- api/controllers/get-count.js
+++ api/controllers/get-count.js
@@ -1,9 +1,12 @@
-import { AppErrorMissing } from '../utils/errors.js';
+import { AppErrorInvalid, AppErrorMissing } from '../utils/errors.js';
 import SdcStat from '../models/sdc-stat.js';
 import { Op } from 'sequelize';
+import countCategories from '../configs/count-categories.js';
 
-export default async function getCount({ category, query: { days } }, res) {
+export default async function ({ category, query: { days } }, res) {
 	if (!days) throw new AppErrorMissing('days');
+	if (!category) throw new AppErrorMissing('category');
+	if (!countCategories.includes(category)) throw new AppErrorInvalid('category');
 
 	const result = await SdcStat.findAll({
 		attributes: ['date', category],

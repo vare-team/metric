679c62a3b6bf1cfbe81f395607bee7f3cf5d056b
Alexander <satan1c@ukr.net>
2023-05-31 17:42:06 +0300

    fix: https://github.com/vare-team/metric/pull/65#discussion_r1209930806

--- api/routes/guilds.js
+++ api/routes/guilds.js
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 import { asyncRoute } from '../utils/errors.js';
 import getGuildStat from '../controllers/get-guild-stat.js';
+import setGuildStat from '../controllers/set-guild-stat.js';
 
 const router = new Router();
 
-router.route('/stat/:guildId').get(asyncRoute(getGuildStat));
+router.route('/stat').get(asyncRoute(getGuildStat));
 
-router.route('/viewed').put(asyncRoute(getGuildStat));
-router.route('/about').put(asyncRoute(getGuildStat));
-router.route('/joined').put(asyncRoute(getGuildStat));
+router.route('/viewed').put(asyncRoute(setGuildStat));
+router.route('/about').put(asyncRoute(setGuildStat));
+router.route('/joined').put(asyncRoute(setGuildStat));
 
 export default router;

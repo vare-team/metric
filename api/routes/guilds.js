73d23f01ecccc849b8edfd4b5c181848e77f87da
MegaVasiliy007 <megavasiliy007@vare.pw>
2023-06-01 20:21:03 +0300

    fix set viewed

--- api/routes/guilds.js
+++ api/routes/guilds.js
@@ -2,10 +2,11 @@ import { Router } from 'express';
 import { asyncRoute } from '../utils/errors.js';
 import getGuildStat from '../controllers/get-guild-stat.js';
 import setGuildStat from '../controllers/set-guild-stat.js';
+import setViewedGuildsStat from '../controllers/set-viewed-guilds-stat.js';
 
 const router = new Router();
 
-router.route('/viewed').put(asyncRoute(setGuildStat));
+router.route('/viewed').put(asyncRoute(setViewedGuildsStat));
 
 router.route('/:guildId').get(asyncRoute(getGuildStat));
 router.route('/:guildId/about').put(asyncRoute(setGuildStat));
